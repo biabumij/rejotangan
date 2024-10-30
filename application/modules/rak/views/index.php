2519ec3ae69a0d9c129abb537aa01fe649370a98
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-10-30 10:38:30 +0700

    first commit

---
+++ application/modules/rak/views/index.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="en" class="fixed">
+
+<head>
+    <?php echo $this->Templates->Header(); ?>
+    <style type="text/css">
+        body {
+            font-family: helvetica;
+        }
+        
+		.mytable thead th {
+		  background-color:	#e69500;
+		  color: #ffffff;
+		  text-align: center;
+		  vertical-align: middle;
+		  padding: 5px;
+		}
+		
+		.mytable tbody td {
+		  padding: 5px;
+		}
+		
+		.mytable tfoot td {
+		  background-color:	#e69500;
+		  color: #FFFFFF;
+		  padding: 5px;
+		}
+
+        button {
+			border: none;
+			border-radius: 5px;
+			padding: 5px;
+			font-size: 12px;
+			text-transform: uppercase;
+			cursor: pointer;
+			color: white;
+			background-color: #2196f3;
+			box-shadow: 0 0 4px #999;
+			outline: none;
+		}
+
+		.ripple {
+			background-position: center;
+			transition: background 0.8s;
+		}
+		.ripple:hover {
+			background: #47a7f5 radial-gradient(circle, transparent 1%, #47a7f5 1%) center/15000%;
+		}
+		.ripple:active {
+			background-color: #6eb9f7;
+			background-size: 100%;
+			transition: background 0s;
+		}
+    </style>
+</head>
+<body>
+    <div class="wrap">
+
+        <?php echo $this->Templates->PageHeader(); ?>
+
+        <div class="page-body">
+            <div class="content">
+                <div class="row animated fadeInUp">
+                    <div class="col-sm-12 col-lg-12">
+                        <div class="panel">
+                            <div class="panel-header">
+                                <h3><b style="text-transform:uppercase;"><?php echo $row[0]->menu_name; ?></b></h3>
+                                <div class="text-left">
+                                    <a href="<?php echo site_url('admin');?>">
+                                    <button class="ripple"><b><i class="fa-solid fa-rotate-left"></i> KEMBALI</b></button></a>
+                                </div>
+                            </div>
+                            <div class="panel-content">
+                                <ul class="nav nav-tabs" role="tablist">
+                                    <li role="presentation" class="active"><a href="#rencana_kerja" aria-controls="rencana_kerja" role="tab" data-toggle="tab" style="border-radius:10px; font-weight:bold;">RENCANA KERJA</a></li>
+                                </ul>
+                                <div class="tab-content">
+                                    <br />
+								    <button style="background-color:#88b93c; border:1px solid black; border-radius:10px; line-height:30px;"><a href="<?php echo site_url('rak/form_rencana_kerja'); ?>"><b style="color:white;">BUAT RENCANA KERJA</b></a></button>
+                                    <div role="tabpanel" class="tab-pane active" id="rencana_kerja">									
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-hover" id="table_rak" style="width:100%">
+                                                <thead>
+                                                    <tr>
+                                                        <th>No.</th>
+														<th>Tanggal</th>
+                                                        <th>Lampiran</th>
+                                                        <th>Edit</th>
+														<th>Hapus</th>
+													</tr>
+                                                </thead>
+                                                <tbody>
+
+                                                </tbody>
+                                                <tfoot>
+                                                   
+                                                </tfoot>
+                                            </table>
+                                        </div>
+									</div>
+		           
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <?php echo $this->Templates->Footer(); ?>
+
+    <script src="<?php echo base_url(); ?>assets/back/theme/vendor/jquery.number.min.js"></script>
+    
+    <script src="<?php echo base_url(); ?>assets/back/theme/vendor/bootbox.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.min.css">
+    <script type="text/javascript" src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/moment.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/daterangepicker.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/back/theme/vendor/daterangepicker/daterangepicker.css">
+    <script src="https://kit.fontawesome.com/591a1bf2f6.js" crossorigin="anonymous"></script>
+    
+    <script type="text/javascript">
+		
+		var table_rak = $('#table_rak').DataTable( {"bAutoWidth": false,
+            ajax: {
+                processing: true,
+                serverSide: true,
+                url: '<?php echo site_url('rak/table_rencana_kerja'); ?>',
+                type: 'POST',
+                data: function(d) {
+                }
+            },
+            responsive: true,
+            paging : false,
+            "deferRender": true,
+            "language": {
+                processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> '
+            },
+            columns: [
+				{
+                    "data": "no"
+                },
+				{
+                    "data": "tanggal_rencana_kerja"
+                },
+                {
+                    "data": "lampiran"
+                },
+                {
+					"data": "edit"
+				},
+				{
+					"data": "actions"
+				},
+            ],
+            "columnDefs": [
+                { "width": "5%", "targets": 0, "className": 'text-center'},
+            ],
+        });
+	
+		function DeleteData(id) {
+        bootbox.confirm("Apakah Anda yakin untuk menghapus data ini ?", function(result) {
+            // console.log('This was logged in the callback: ' + result); 
+            if (result) {
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo site_url('rak/delete_rencana_kerja'); ?>",
+                    dataType: 'json',
+                    data: {
+                        id: id
+                    },
+                    success: function(result) {
+                        if (result.output) {
+                            table_rak.ajax.reload();
+                            bootbox.alert('<b>DELETED</b>');
+                        } else if (result.err) {
+                            bootbox.alert(result.err);
+                        }
+                    }
+                });
+            }
+        });
+        }
+
+    </script>
+
+</body>
+
+</html>
