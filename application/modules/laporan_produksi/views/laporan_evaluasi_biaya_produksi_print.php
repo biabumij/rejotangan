1090c6f0cc9a20ee07049f42c0edf3874df92e9b
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2024-11-05 15:12:59 +0700

    update

--- application/modules/laporan_produksi/views/laporan_evaluasi_biaya_produksi_print.php
+++ application/modules/laporan_produksi/views/laporan_evaluasi_biaya_produksi_print.php
@@ -705,10 +705,12 @@
 
 		$batching_plant = $harsat_batching_plant * $total_volume;
 		$pemeliharaan_batching_plant = $harsat_pemeliharaan_batching_plant * $total_volume;
-		$penyusutan_batching_plant = $batching_plant - $pemeliharaan_batching_plant;
+		//$penyusutan_batching_plant = $batching_plant - $pemeliharaan_batching_plant;
+		$penyusutan_batching_plant = 0;
 		$wheel_loader = ($harsat_wheel_loader * $vol_wheel_loader) + $total_nilai_pemeliharaan_wheel_loader;
 		$pemeliharaan_wheel_loader = $harsat_pemeliharaan_wheel_loader * $vol_pemeliharaan_wheel_loader;
-		$penyusutan_wheel_loader = $wheel_loader - $pemeliharaan_wheel_loader;
+		//$penyusutan_wheel_loader = $wheel_loader - $pemeliharaan_wheel_loader;
+		$penyusutan_wheel_loader = 0;
 		$truck_mixer = $harsat_truck_mixer * $total_volume;
 		$excavator = $harsat_excavator * $total_volume;
 		$transfer_semen = $harsat_transfer_semen * $total_volume;
@@ -779,6 +781,7 @@
 		$rap_bua = $this->db->select('sum(det.jumlah) as total')
 		->from('rap_bua rap')
 		->join('rap_bua_detail det','rap.id = det.rap_bua_id','left')
+		->where("det.coa <> 131 ")
 		->where("rap.status = 'PUBLISH'")
 		->where("rap.tanggal_rap_bua < '$date2'")
 		->order_by('rap.tanggal_rap_bua','asc')->limit(1)
@@ -816,7 +819,7 @@
 		$total_nilai_realisasi_bua = $overhead_biaya['total'] + $overhead_jurnal['total'];
 
 		$total_volume_rap_bua = $total_volume;
-		$total_nilai_rap_bua = $rap_bua / 24;
+		$total_nilai_rap_bua = $rap_bua / 3;
 		$total_harsat_rap_bua = $total_nilai_rap_bua / $total_volume_rap_bua;
 		
 		$total_volume_realisasi_bua = $total_volume;
