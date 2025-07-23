9abd4f5b4d6c20c5b354f118d98b1be9b735ca2b
Bayu <ginanjar.bayubimantoro@biabumijayendra.com>
2025-07-23 09:12:04 +0700

    update

--- application/modules/produksi/views/rakor.php
+++ application/modules/produksi/views/rakor.php
@@ -3,18 +3,24 @@
         <input type="text" id="filter_date_pemakaian" name="filter_date" class="form-control dtpickerangepemakaian" autocomplete="off" placeholder="Filter By Date">
     </div>
     <!--<div class="col-sm-1">
-        <button type="submit" class="btn btn-default" style="border-radius:10px; font-weight:bold;">PRINT</button>
+        <button type="submit" class="btn btn-default" style="border-radius:5px; font-weight:bold;">PRINT</button>
     </div>-->
 </form>
 <?php
-    if(in_array($this->session->userdata('admin_group_id'), array(1,2,3,4,5))){
-    ?>
+$admin_id = $this->session->userdata('admin_id');
+$approval = $this->db->select('*')
+->from('tbl_admin')
+->where("admin_id = $admin_id ")
+->get()->row_array();
+$kunci_rakor =  $approval['kunci_rakor'];
+?>
+<?php if($kunci_rakor == 1){?>
     <div class="col-sm-5">
-    <button style="background-color:#88b93c; border:1px solid black; border-radius:10px; line-height:30px;"><a href="<?php echo site_url('produksi/form_rakor'); ?>"><b style="color:white;">BUAT KUNCI</b></a></button>
+    <button style="background-color:#88b93c; border-radius:5px; line-height:30px;"><a href="<?php echo site_url('produksi/form_rakor'); ?>"><b style="color:white;">BUAT KUNCI</b></a></button>
     </div>
-    <?php
-    }
-    ?>
+<?php
+}
+?>
 <br />
 <br />
 <div class="table-responsive">
