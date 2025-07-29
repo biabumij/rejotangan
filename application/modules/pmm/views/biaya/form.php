9f91b1567ce0e87589bcbd39b274e19a2d0dcc4f
biabumij <108206630+biabumij@users.noreply.github.com>
2025-07-29 09:58:16 +0700

    Update form.php

--- application/modules/pmm/views/biaya/form.php
+++ application/modules/pmm/views/biaya/form.php
@@ -239,9 +239,7 @@
         var form_control = '';
     </script>
     <?php echo $this->Templates->Footer();?>
-
     <script src="<?php echo base_url();?>assets/back/theme/vendor/jquery.number.min.js"></script>
-    
     <script src="<?php echo base_url();?>assets/back/theme/vendor/daterangepicker/moment.min.js"></script>
     <script src="<?php echo base_url();?>assets/back/theme/vendor/daterangepicker/daterangepicker.js"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/back/theme/vendor/daterangepicker/daterangepicker.css">
@@ -261,9 +259,9 @@
               format: 'DD-MM-YYYY'
             },
             minDate: '<?php echo $last_opname;?>',
-			//maxDate: moment().add(+0, 'd').toDate(),
+			maxDate: moment().add(+0, 'd').toDate(),
             //minDate: moment().startOf('month').toDate(),
-			maxDate: moment().endOf('month').toDate(),
+			//maxDate: moment().endOf('month').toDate(),
         });
         $('.dtpicker').on('apply.daterangepicker', function(ev, picker) {
               $(this).val(picker.startDate.format('DD-MM-YYYY'));
