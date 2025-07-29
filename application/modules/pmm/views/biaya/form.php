8c9d4b242287936d58cbb45cd41e6012793e6777
biabumij <108206630+biabumij@users.noreply.github.com>
2025-07-29 10:01:14 +0700

    Update form.php

--- application/modules/pmm/views/biaya/form.php
+++ application/modules/pmm/views/biaya/form.php
@@ -244,7 +244,6 @@
     <script src="<?php echo base_url();?>assets/back/theme/vendor/daterangepicker/daterangepicker.js"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/back/theme/vendor/daterangepicker/daterangepicker.css">
     <script src="<?php echo base_url();?>assets/back/theme/vendor/bootbox.min.js"></script>
-
     <script type="text/javascript">
         <?php
         $kunci_rakor = $this->db->select('date')->order_by('date','desc')->limit(1)->get_where('kunci_rakor')->row_array();
@@ -501,13 +500,8 @@
                     }
                 }
             });
-
             event.preventDefault();
-            
         });
-        
     </script>
-
-
 </body>
 </html>
